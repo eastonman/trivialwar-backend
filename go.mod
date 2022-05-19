d5f7c90f66e182d2bb7865ea4a7e7e9b5ec76239
Easton Man <manyang.me@outlook.com>
2022-05-19 23:23:02 +0800

    feat: init project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/eastonman/trivialwar-backend
+
+go 1.18
+
+require github.com/gorilla/websocket v1.5.0
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+var socketDir = "/socket"
+
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home page, Websocket at"+socketDir)
+}
+
+func main() {
+
+	// Setup signal monitor
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt)
+
+	http.HandleFunc(socketDir, wsHandler)
+	http.HandleFunc("/", homePageHandler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
